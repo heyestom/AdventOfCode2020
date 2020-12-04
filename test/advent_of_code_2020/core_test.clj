0bcbdcfcc4109e5b1cf1bd4d1d5d9a4ba3d5e4c0
theyes@thoughtworks.com <theyes@thoughtworks.com>
2020-12-04 18:07:12 +0000

    Advent of code 2020 task 1

---
+++ test/advent_of_code_2020/core_test.clj
@@ -0,0 +1,18 @@
+(ns advent-of-code-2020.core-test
+  (:require [clojure.test :refer :all]
+            [advent-of-code-2020.core :refer :all]))
+
+(deftest finding-two-nums-with-summate-to-target
+  (testing "when two numbers in a list sumate to the target val returns them in a tuple"
+    (let [input [2 3 8]
+          target 10]
+      (is (= [2 8] (find-tupple-suming target input)))))
+
+  (testing "when no numbers in the input sum to the target val returns nil"
+    (let [input [2 3 1]
+          target 1000]
+      (is (nil? (find-tupple-suming target input)))))
+  )
+
+
+
